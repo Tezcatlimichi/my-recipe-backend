a546872a826438a499baac80d62c13133b489373
Kathya Castaneda <kathyakathya.kc@gmail.com>
2024-09-16 16:46:10 -0400

    server is up

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 const express = require('express')
+const cors = require('cors')
+const logger = require('morgan')
+
+const app = express()
+
+const PORT = process.env.PORT || 3001
+
+app.use(cors())
+app.use(express.json())
+app.use(logger('dev'))
+app.use(express.urlencoded({ extended: true }))
+app.get('/', (req, res) => res.json({ message: 'Server is up silly goose' }))
+
+app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
